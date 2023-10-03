b708bb5ef1b57886cea189d998d7c1be90392a8f
chasacademy-sandra-larsson <111293794+chasacademy-sandra-larsson@users.noreply.github.com>
2023-10-03 13:56:25 +0200

    added ts-project

---
+++ dist/repetition.js
@@ -0,0 +1,17 @@
+"use strict";
+{
+    let name = 'John';
+    let age = 25;
+    let isAdult = true;
+    let unknown = undefined;
+    let nothing = null;
+    // Symbol, Bigint
+    let hobbies = ['Cooking', 'Sports'];
+    const person = {
+        name: "Sandra",
+        age: 27
+    };
+    function sayHello(greet) {
+        return greet;
+    }
+}
---
+++ src/repetition.ts
@@ -0,0 +1,30 @@
+{
+let name: string = 'John';
+let age: number = 25;
+let isAdult: boolean = true;
+let unknown: undefined = undefined;
+let nothing: null = null;
+
+// Symbol, Bigint
+
+let hobbies: string[] = ['Cooking', 'Sports'];
+
+interface Person  {
+    name: string | number,
+    age: number
+}
+
+const person: Person = {
+    name: "Sandra",
+    age: 27
+}
+
+
+
+function sayHello(greet: string): string{
+
+    return greet
+}
+
+
+}
